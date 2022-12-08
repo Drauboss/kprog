da64c7beb6124240407ffb25281b4a1f2be0baed
byilmazer <byilmazer@stud.hs-heilbronn.de>
2022-12-08 16:08:19 +0100

    ex11 aufg 1 save done

--- src/main/java/prog/ex11/solution/saveandload/factory/PlainTextPersistenceFactory.java
+++ src/main/java/prog/ex11/solution/saveandload/factory/PlainTextPersistenceFactory.java
@@ -1,10 +1,19 @@
 package prog.ex11.solution.saveandload.factory;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 import prog.ex11.exercise.saveandload.factory.PersistenceFactory;
 import prog.ex11.exercise.saveandload.factory.WrongOrderFormatException;
 import prog.ex11.exercise.saveandload.pizzadelivery.Order;
+import prog.ex11.solution.saveandload.pizzadelivery.SimpleOrder;
+
 
 /**
  * Simple and straight-forward implementation of the PersistenceFactory interface.
@@ -17,10 +26,98 @@ public class PlainTextPersistenceFactory implements PersistenceFactory {
   @Override
   public void save(final File file, final Order order) throws IOException {
 
+    List<String> orderTokens = new ArrayList<>();
+    List<String> pizzaTokens = new ArrayList<>();
+
+
+    String orderId = String.valueOf(order.getOrderId());
+    String ordervalue = String.valueOf(order.getValue());
+    String numOfPizzas = String.valueOf(order.getPizzaList().size());
+
+    orderTokens.add(orderId);
+    orderTokens.add(";");
+    orderTokens.add(ordervalue);
+    orderTokens.add(";");
+    orderTokens.add(numOfPizzas);
+
+    for (int i = 0; i < order.getPizzaList().size(); i++) {
+      String pizzaId = String.valueOf(order.getPizzaList().get(i).getPizzaId());
+      String pizzaValue = String.valueOf(order.getPizzaList().get(i).getPrice());
+      String pizzaSize = String.valueOf(order.getPizzaList().get(i).getSize());
+
+
+      pizzaTokens.add(pizzaId);
+      pizzaTokens.add(";");
+      pizzaTokens.add(pizzaValue);
+      pizzaTokens.add(";");
+      pizzaTokens.add(pizzaSize);
+
+
+
+      for (int j = 0; j < order.getPizzaList().get(i).getToppings().size(); j++) {
+        String topping = String.valueOf(order.getPizzaList().get(i).getToppings().get(j));
+        pizzaTokens.add(";");
+        pizzaTokens.add(topping);
+      }
+
+      //new line only after last topping
+      if (i != order.getPizzaList().size() - 1) {
+        pizzaTokens.add("\n");
+      }
+    }
+
+
+
+    try (PrintWriter wr = new PrintWriter(new FileWriter(file))) {
+      //print order infos
+      for (int i = 0; i < orderTokens.size(); i++) {
+        wr.print(orderTokens.get(i));
+      }
+      wr.println();
+
+      //print Pizza infos
+      for (int i = 0; i < pizzaTokens.size(); i++) {
+        wr.print(pizzaTokens.get(i));
+      }
+
+    }
+
   }
 
   @Override
   public Order load(final File file) throws IOException, WrongOrderFormatException {
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+
+      List<String> orderTokens = new ArrayList<>();
+      List<String> pizzaTokens = new ArrayList<>();
+
+      String line = reader.readLine();
+      StringTokenizer tokenizer = new StringTokenizer(line, ";");
+      while (tokenizer.hasMoreElements()) {
+        orderTokens.add(tokenizer.nextToken());
+      }
+
+      System.out.println("order infos");
+      System.out.println(orderTokens);
+
+      String orderId = orderTokens.get(0);
+      String ordervalue = orderTokens.get(1);
+      String numOfPizzas = orderTokens.get(2);
+
+
+      for (int i = 0; i < 22; i++) {
+        Order order = new SimpleOrder();
+        //TODO: Frage set Id methode in interface hinzufügen
+
+      }
+
+
+    }
+
+
+
+
     return null;
   }
 }
--- src/main/java/prog/ex11/solution/saveandload/pizzadelivery/SimpleOrder.java
+++ src/main/java/prog/ex11/solution/saveandload/pizzadelivery/SimpleOrder.java
@@ -1,29 +1,50 @@
 package prog.ex11.solution.saveandload.pizzadelivery;
 
-
+import java.util.ArrayList;
 import java.util.List;
 import prog.ex11.exercise.saveandload.pizzadelivery.Order;
 import prog.ex11.exercise.saveandload.pizzadelivery.Pizza;
+
 
 /**
  * Simple and straight-forward implementation of the Order interface.
  */
 public class SimpleOrder implements Order {
   private static final org.slf4j.Logger logger =
-          org.slf4j.LoggerFactory.getLogger(SimpleOrder.class);
+      org.slf4j.LoggerFactory.getLogger(SimpleOrder.class);
+
+
+
+  private int id;
+  private static int idCounter = 0;
+  List<Pizza> pizzaList = new ArrayList<>();
+
+  public SimpleOrder() {
+    id = ++idCounter;
+  }
 
   @Override
   public int getOrderId() {
-    return 0;
+    return id;
+  }
+
+
+  public void setOrderId(int id) {
+    this.id = id;
   }
 
   @Override
   public List<Pizza> getPizzaList() {
-    return null;
+    return pizzaList;
   }
 
   @Override
   public int getValue() {
-    return 0;
+    int value = 0;
+    for (Pizza p : pizzaList) {
+      value = value + p.getPrice();
+    }
+    return value;
   }
+
 }
