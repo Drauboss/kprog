6bf69eb9ac18b1713f568bef589d1d5411127c93
berkay <byilmazer@stud.hs-heilbronn.de>
2023-01-09 18:54:38 +0100

    fsdf

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -22,13 +22,7 @@ module kprog.main {
   exports prog.javafx.main;
   exports prog.ex10.solution.javafx4pizzadelivery.gui;
   exports prog.ex15.monolingual.gui;
-<<<<<<< HEAD
-  exports prog.ex15.solution.i18ncountries.gui;
-  exports prog.ex15.solution.i18ncountries;
-  exports prog.ex15.exercise.i18ncountries;
-=======
   exports examples.rmi.shapes;
->>>>>>> d5d50a3192d7b103eb2036422e6be285303a1560
   opens examples.javafx.fxml to javafx.fxml;
   opens prog.ex10.solution.javafx4pizzadelivery.gui to javafx.fxml;
   exports livesession.snake.javafx;
