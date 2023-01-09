d5d50a3192d7b103eb2036422e6be285303a1560
byilmazer <byilmazer@stud.hs-heilbronn.de>
2023-01-09 18:32:16 +0100

    cleaned

--- src/main/java/prog/ex15/solution/i18ncountries/I18nKnowledgeGenerator.java
+++ src/main/java/prog/ex15/solution/i18ncountries/I18nKnowledgeGenerator.java
@@ -1,7 +1,11 @@
 package prog.ex15.solution.i18ncountries;
 
+import java.text.MessageFormat;
+import java.util.Locale;
+import prog.ex15.exercise.i18ncountries.Category;
 import prog.ex15.exercise.i18ncountries.CountryKnowledgeContainer;
 import prog.ex15.exercise.i18ncountries.KnowledgeGenerator;
+import prog.ex15.exercise.i18ncountries.TypicalCountry;
 
 /**
  * Simple, straight-forward implementation of the KnowledgeGenerator interface for multiple
@@ -11,8 +15,148 @@ public class I18nKnowledgeGenerator implements KnowledgeGenerator {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(I18nKnowledgeGenerator.class);
 
+
+  SingletonConfiguration singletonConfiguration = SingletonConfiguration.getInstance();
+  Locale locale;
+  CountryKnowledgeContainer container = new CountryKnowledgeContainer();
+
+
+  private String printMessage(final String bundleKey, String firstParam, String secondParam) {
+    return MessageFormat.format(singletonConfiguration.getMessageBundle().getString(bundleKey), firstParam, secondParam);
+  }
+  private String printMessage(final String bundleKey, String firstParam) {
+    return MessageFormat.format(singletonConfiguration.getMessageBundle().getString(bundleKey), firstParam);
+  }
+
+  public void addUkKnowledge() {
+
+    System.out.println("add uk knowledge");
+    //System.out.println(singletonConfiguration.getLocale());
+    //System.out.println(Locale.getDefault());
+    //Locale.setDefault(Locale.US);
+    //System.out.println(Locale.getDefault());
+    logger.info(String.valueOf(singletonConfiguration.getLocale()));
+    String velocity = singletonConfiguration.getTypicalBundle().getString(TypicalCountry.VELOCITY);
+    String velocityUnit = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.VELOCITY_UNIT);
+    String population = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.POPULATION);
+    String mostImportantHolidayDate = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_IMPORTANT_HOLIDAY_DATE);
+    String mostImportantHolidayName = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_IMPORTANT_HOLIDAY_NAME);
+    String mostFamousMeal = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_FAMOUS_MEAL);
+
+    container.addKnowledge(Category.TRAFFIC,
+        printMessage("traffic.maximum.speed.highways", velocity, velocityUnit));
+    container.addKnowledge(Category.FOOD,
+        printMessage("food.most.prominent.food", mostFamousMeal));
+    container.addKnowledge(Category.HOLIDAYS,
+        printMessage("holiday.most.important.holiday", mostImportantHolidayName, mostImportantHolidayDate));
+    container.addKnowledge(Category.STATISTICS,
+        printMessage("statistics.population", population));
+
+  }
+
+  public void addGermanyKnowledge() {
+    String velocity = singletonConfiguration.getTypicalBundle().getString(TypicalCountry.VELOCITY);
+    String velocityUnit = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.VELOCITY_UNIT);
+    String population = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.POPULATION);
+    String mostImportantHolidayDate = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_IMPORTANT_HOLIDAY_DATE);
+    String mostImportantHolidayName = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_IMPORTANT_HOLIDAY_NAME);
+    String mostFamousMeal = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_FAMOUS_MEAL);
+
+    container.addKnowledge(Category.TRAFFIC,
+        printMessage("traffic.maximum.speed.highways", velocity, velocityUnit));
+    container.addKnowledge(Category.FOOD,
+        printMessage("food.most.prominent.food", mostFamousMeal));
+    container.addKnowledge(Category.HOLIDAYS,
+        printMessage("holiday.most.important.holiday", mostImportantHolidayName, mostImportantHolidayDate));
+    container.addKnowledge(Category.STATISTICS,
+        printMessage("statistics.population", population));
+  }
+
+  public void addDenmarkKnowledge() {
+    String velocity = singletonConfiguration.getTypicalBundle().getString(TypicalCountry.VELOCITY);
+    String velocityUnit = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.VELOCITY_UNIT);
+    String population = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.POPULATION);
+    String mostImportantHolidayDate = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_IMPORTANT_HOLIDAY_DATE);
+    String mostImportantHolidayName = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_IMPORTANT_HOLIDAY_NAME);
+    String mostFamousMeal = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_FAMOUS_MEAL);
+
+    container.addKnowledge(Category.TRAFFIC,
+        printMessage("traffic.maximum.speed.highways", velocity, velocityUnit));
+    container.addKnowledge(Category.FOOD,
+        printMessage("food.most.prominent.food", mostFamousMeal));
+    container.addKnowledge(Category.HOLIDAYS,
+        printMessage("holiday.most.important.holiday", mostImportantHolidayName, mostImportantHolidayDate));
+    container.addKnowledge(Category.STATISTICS,
+        printMessage("statistics.population", population));
+  }
+
+  public void addNetherlandsKnowledge() {
+    String velocity = singletonConfiguration.getTypicalBundle().getString(TypicalCountry.VELOCITY);
+    String velocityUnit = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.VELOCITY_UNIT);
+    String population = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.POPULATION);
+    String mostImportantHolidayDate = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_IMPORTANT_HOLIDAY_DATE);
+    String mostImportantHolidayName = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_IMPORTANT_HOLIDAY_NAME);
+    String mostFamousMeal = singletonConfiguration.getTypicalBundle()
+        .getString(TypicalCountry.MOST_FAMOUS_MEAL);
+
+    container.addKnowledge(Category.TRAFFIC,
+        printMessage("traffic.maximum.speed.highways", velocity, velocityUnit));
+    container.addKnowledge(Category.FOOD,
+        printMessage("food.most.prominent.food", mostFamousMeal));
+    container.addKnowledge(Category.HOLIDAYS,
+        printMessage("holiday.most.important.holiday", mostImportantHolidayName, mostImportantHolidayDate));
+    container.addKnowledge(Category.STATISTICS,
+        printMessage("statistics.population", population));
+  }
+
+  public CountryKnowledgeContainer changeContainerTo(Locale locale) {
+
+    container.clear();
+    logger.info(String.valueOf(locale));
+    logger.info(String.valueOf(locale.getCountry()));
+    switch (locale.getCountry()) {
+      case "GB":
+        addUkKnowledge();
+        break;
+      case "DE":
+        addGermanyKnowledge();
+        break;
+      case "NL":
+        addNetherlandsKnowledge();
+        break;
+      case "DK":
+        addDenmarkKnowledge();
+        break;
+      default:
+        logger.info("wrong locale switch case");
+
+    }
+
+    return container;
+  }
+
   @Override
   public CountryKnowledgeContainer fillContainer() {
-    return null;
+    addGermanyKnowledge();
+    return container;
   }
 }
--- src/main/java/prog/ex15/solution/i18ncountries/SingletonConfiguration.java
+++ src/main/java/prog/ex15/solution/i18ncountries/SingletonConfiguration.java
@@ -1,6 +1,8 @@
 package prog.ex15.solution.i18ncountries;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
@@ -14,40 +16,82 @@ public class SingletonConfiguration implements Configuration {
   private static final org.slf4j.Logger logger =
       org.slf4j.LoggerFactory.getLogger(SingletonConfiguration.class);
 
+
+  private static final SingletonConfiguration self = new SingletonConfiguration();
   public static SingletonConfiguration getInstance() {
-    return null;
+    return self;
   }
 
+  Map<Country, Locale> countryLocaleMap = new HashMap<>();
+  Locale locale;
+  PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+  ResourceBundle infos;
   @Override
   public Locale getLocale() {
-    return null;
+    return locale;
   }
 
   @Override
   public void setLocale(final Locale locale) {
-
+    Locale oldLocal = this.locale;
+    Locale newLocale = locale;
+    this.locale = locale;
+    logger.info("proberty changed to " + locale.toString());
+    propertyChangeSupport.firePropertyChange("newLocale", oldLocal, locale);
+    //System.out.println("proberty changed to " + locale.toString());
   }
 
   @Override
   public ResourceBundle getTypicalBundle() {
-    return null;
+    switch (getLocale().getLanguage()) {
+      case "en":
+        infos = ResourceBundle.getBundle("prog.ex15.solution.i18ncountries.InfoBundle", Locale.UK);
+        break;
+      case "de":
+        infos = ResourceBundle.getBundle("prog.ex15.solution.i18ncountries.InfoBundle", Locale.GERMANY);
+        break;
+      case "dk":
+        infos = ResourceBundle.getBundle("prog.ex15.solution.i18ncountries.InfoBundle", getCountry2LocaleMap().get(Country.DENMARK));
+        break;
+      case "nl":
+        infos = ResourceBundle.getBundle("prog.ex15.solution.i18ncountries.InfoBundle", getCountry2LocaleMap().get(Country.NETHERLANDS));
+        break;
+
+      default:
+        logger.info("fgasgdasgdsafgasggdsaqg");
+    }
+    //ResourceBundle infos = ResourceBundle.getBundle("prog.ex15.solution.i18countries.InfoBundle", Locale.GERMANY);
+    //ResourceBundle infos = ResourceBundle.getBundle("prog/ex15/solution/i18countries/InfoBundle", Locale.UK);
+    //ResourceBundle infos = ResourceBundle.getBundle("src/main/java/prog/ex15/solution/i18countries/infoBundle2.java", Locale.UK);
+    System.out.println(infos);
+    return infos;
   }
 
   @Override
   public ResourceBundle getMessageBundle() {
-    return null;
+    ResourceBundle messages = ResourceBundle.getBundle("bundles/i18ncountries", getLocale());
+    logger.info(String.valueOf(messages));
+
+    return messages;
   }
 
   @Override
   public Map<Country, Locale> getCountry2LocaleMap() {
-    return null;
+    countryLocaleMap.put(Country.DENMARK, new Locale("dk", "DK"));
+    countryLocaleMap.put(Country.GERMANY, Locale.GERMANY);
+    countryLocaleMap.put(Country.NETHERLANDS, new Locale("nl", "NL"));
+    countryLocaleMap.put(Country.ENGLAND, Locale.UK);
+    return countryLocaleMap;
   }
 
   @Override
   public void addPropertyChangeListener(final PropertyChangeListener listener) {
+    propertyChangeSupport.addPropertyChangeListener(listener);
   }
 
   @Override
   public void removePropertyChangeListener(final PropertyChangeListener listener) {
+    propertyChangeSupport.removePropertyChangeListener(listener);
   }
 }
